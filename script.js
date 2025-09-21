dab22738757a31a1b31c549be5660c9845b29f7b
pranavbhat777 <146847778+pranavbhat777@users.noreply.github.com>
2025-09-21 11:52:52 +0530

    script.js

--- script.js
+++ script.js
@@ -1,13 +1,8 @@
 // Script.js
-// create a new QRCode instance
 let qrcode = new QRCode(
     document.querySelector(".qrcode")
 );
-// Initial QR code generation
-// with a default message
-qrcode.makeCode("Why did you scan me?");
-// Function to generate QR
-// code based on user input
+qrcode.makeCode("Type in textbox then scan me");
 function generateQr() {
     if (
         document.querySelector("input")
